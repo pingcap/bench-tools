c3cf7ea91d3698d0ce9e7f8529979d2436218a7d
lishihai9017 <lishihai@pingcap.com>
2017-06-06 16:06:29 +0800

    support config key len & value len & batch size

--- rocksdb-in-tikv/src/main.rs
+++ rocksdb-in-tikv/src/main.rs
@@ -31,25 +31,25 @@ use sim::key::{KeyGen, RepeatKeyGen, IncreaseKeyGen, RandomKeyGen};
 use sim::val::ConstValGen;
 use sim::cf::{cf_default_w, cf_lock_w, cf_write_w, cf_raft_w};
 
-const KEY_LEN: usize = 32;
-const VALUE_LEN: usize = 128;
-const BATCH_SIZE: usize = 128;
+const DEFAULT_KEY_LEN: usize = 32;
+const DEFAULT_VALUE_LEN: usize = 128;
+const DEFAULT_BATCH_SIZE: usize = 128;
 
 fn run() -> Result<usize, String> {
     let app = App::new("Rocksdb in TiKV")
         .author("PingCAP")
         .about("Benchmark of rocksdb in the sim-tikv-env")
-        .arg(Arg::with_name("nosyscheck")
+        .arg(Arg::with_name("skip_sys_check")
             .short("N")
             .takes_value(false)
             .help("skip system check")
             .required(false))
-        .arg(Arg::with_name("db")
+        .arg(Arg::with_name("db_path")
             .short("db")
             .takes_value(true)
             .help("rocksdb path")
             .required(true))
-        .arg(Arg::with_name("cfg")
+        .arg(Arg::with_name("config")
             .short("c")
             .takes_value(true)
             .help("toml config file")
@@ -59,7 +59,22 @@ fn run() -> Result<usize, String> {
             .takes_value(true)
             .help("request count")
             .required(true))
-        .arg(Arg::with_name("keygen")
+        .arg(Arg::with_name("key_len")
+            .short("K")
+            .long("key_len")
+            .help("set key len")
+            .required(false))
+        .arg(Arg::with_name("val_len")
+            .short("V")
+            .long("val_len")
+            .help("set value len")
+            .required(false))
+        .arg(Arg::with_name("batch_size")
+            .short("B")
+            .long("batch_size")
+            .help("set batch size")
+            .required(false))
+        .arg(Arg::with_name("key_gen")
             .short("k")
             .help("key generator, [repeat, increase, random]")
             .default_value("random")
@@ -73,36 +88,59 @@ fn run() -> Result<usize, String> {
 
     let matches = app.clone().get_matches();
 
-    if !matches.is_present("nosyscheck") {
+    if !matches.is_present("skip_sys_check") {
         if let Err(e) = env::check::check_system_config() {
             return Err(format!("system config not satisfied: {}\n", e));
         }
     }
 
-    let db_path = matches.value_of("db").unwrap();
-    let cfg = matches.value_of("cfg").unwrap();
+    let db_path = matches.value_of("db_path").unwrap();
+    let cfg = matches.value_of("config").unwrap();
     let (opt_db, opt_cf) = try!(dbcfg::get_db_config(cfg));
     let db = try!(DB::open_cf(opt_db, db_path, &["default"], &[&opt_cf]));
 
-    let count = matches.value_of("count").unwrap();
-    let count: usize = match count.parse() {
-        Ok(v) => v,
-        Err(_) => return Err("-n <count>: is not a number".to_owned()),
+    let count = match matches.value_of("count") {
+        Some(v) => match v.parse() {
+            Ok(v) => v,
+            Err(count) => return Err(format!("{} is not a number", count)),
+        },
+        None => DEFAULT_KEY_LEN,
+    };
+    let key_len = match matches.value_of("key_len") {
+        Some(v) => match v.parse() {
+            Ok(v) => v,
+            Err(key_len) => return Err(format!("{} is not a number", key_len)),
+        },
+        None => DEFAULT_VALUE_LEN,
+    };
+    let val_len = match matches.value_of("val_len") {
+        Some(v) => match v.parse() {
+            Ok(v) => v,
+            Err(val_len) => return Err(format!("{} is not a number", val_len)),
+        },
+        None => DEFAULT_VALUE_LEN,
+    };
+    let batch_size = match matches.value_of("batch_size") {
+        Some(v) => match v.parse() {
+            Ok(v) => v,
+            Err(batch_size) => return Err(format!("{} is not a number", batch_size)),
+        }, 
+        None => DEFAULT_BATCH_SIZE,
     };
 
-    let mut key_gen: Box<KeyGen> = match matches.value_of("keygen").unwrap() {
-        "repeat" => Box::new(RepeatKeyGen::new(KEY_LEN, count)),
-        "increase" => Box::new(IncreaseKeyGen::new(KEY_LEN, count)),
-        "random" => Box::new(RandomKeyGen::new(KEY_LEN, count)),
-        key_gen => return Err(format!("{} is not a valid key_gen", key_gen)),
+    let mut key_gen: Box<KeyGen> = match matches.value_of("key_gen").unwrap() {
+        "repeat" => Box::new(RepeatKeyGen::new(key_len, count)),
+        "increase" => Box::new(IncreaseKeyGen::new(key_len, count)),
+        "random" => Box::new(RandomKeyGen::new(key_len, count)),
+        invalid => return Err(format!("{} is not a valid key_gen", invalid)),
     };
-    let mut val_gen = ConstValGen::new(VALUE_LEN);
+    let mut val_gen = ConstValGen::new(val_len);
 
     let res = match matches.subcommand() {
         ("cf", Some(cf)) => {
             match cf.subcommand_name().unwrap() {
-                "default" => cf_default_w(db, &mut *key_gen, &mut val_gen, BATCH_SIZE),
-                "lock" => cf_lock_w(db, &mut *key_gen, &mut val_gen, BATCH_SIZE),
+                "default" => cf_default_w(db, &mut *key_gen, &mut val_gen, batch_size),
+                "lock" => cf_lock_w(db, &mut *key_gen, &mut val_gen, batch_size),
                 "write" => cf_write_w(db),
                 "raft" => cf_raft_w(db),
                 _ => help_err(app),
